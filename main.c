024938d15dab21f121e463c20e9da8cff2f8dc2d
Valentim-git <40304@ufp.edu.pt>
2021-11-26 11:46:52 +0000

    AC

--- main.c
+++ main.c
@@ -3,67 +3,78 @@
 #include "string.h"
 #include "time.h"
 
+void gerarCodigo(int codSecreto[]){
+    for (int i = 0; i < 4; i++) {//gera os numeros aleatórios
+        codSecreto[i] = 0 + rand() % 6;
+    }
+}
+void printAleatorios(int codSecreto[]){
+    for (int i = 0; i < 4; ++i) {//imprime os numeros aleatorios
+        printf("%d", codSecreto[i]);
+    }
+    printf("\n");
+}
 
-
-void randomColor (char codSecreto[4][10]) {
-
-    int gerarCor;
-    for (int i = 0; i < 4; i++) {
-        gerarCor = 1 + rand() % 6;
-        switch (gerarCor) {
-            case 1:
-                strcpy(codSecreto[i], "B");
-                break;
-            case 2:
-                strcpy(codSecreto[i], "G");
-                break;
-            case 3:
-                strcpy(codSecreto[i], "R");
-                break;
-            case 4:
-                strcpy(codSecreto[i], "Y");
-                break;
-            case 5:
-                strcpy(codSecreto[i], "W");
-                break;
-            case 6:
-                strcpy(codSecreto[i], "O");
-                break;
+void imprimir_matriz(char matriz[]){
+    int posicao = 0;
+    printf("%s\n",matriz);
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            printf("%c",matriz[posicao]);
+            printf("  ");
+            posicao ++;
         }
+        printf("\n");
+    }
+}
+void contador(char matriz[]){
+    int contador= strlen(matriz);
+    printf("tem %d ",contador);
+}
+void jogadafunct(char matriz[],int jogada){
+    printf("Pode dar uma combinação de 4 letas BGRYWO\n");
+    //scanf("%c %c %c %c\n",&matriz[jogada],&matriz[jogada+1],&matriz[jogada+2],&matriz[jogada+3]);
+    scanf("%c",&matriz[jogada]);
+    scanf("%c",&matriz[jogada+1]);
+    scanf("%c",&matriz[jogada+2]);
+    scanf("%c",&matriz[jogada+3]);
+    jogada=jogada+4;
+    imprimir_matriz(matriz);
+}
+
+void jogo(char matriz[],int jogada){
+    while(jogada!=40){
+        jogadafunct(matriz,jogada);
     }
 }
 
-void advinharCombinacao (char advinharCod[4][10]) {
-
-    printf("Introduza a Sequencia: \n");
-    for (int i = 0; i < 4; i++) {
-        scanf("%s", advinharCod[i]);
-    }
-}
-
-void verificarCombinacao (char codSecreto[4][10], char advinharCod[4][10]) {
-
-    int pinPreto = 0, pinBranco = 0;
-    for (int i = 0; i < 4; i++) {
-        // se estiver certo incrementar pinPreto
-        if (strcmp(advinharCod[i], codSecreto[i]) == 0) {
-            pinPreto++;
-        }
-    }
-
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            // se estiver certo mas em posição difrente incrementar pinBranco
-            if(strcmp(codSecreto[i], advinharCod[j]) == 0) {
-                pinBranco++;
-            }
-        }
-    }
-}
+//int verificarConbinacao(char matriz[],int jogada,int codSecreto[]){}
+//int CCPC (){//CORES CERTAS POSIÇÃO CERTA}
+//int CCPE(){//CORES CERTAS POSIÇÃO ERRADA}
 
 
 int main() {
+    int jogada=0;
+    char matriz[41]={"0000000000000000000000000000000000000000"};
+    char code[]="BGRYWO";
+    int codSecreto[4];//tem o codigo mas em numeros que precisa traduzir para letras
+    //char cod_Secreto[4];//tem o codigo dos numeros ja traduzido em letras
     srand(time(NULL));
+
+    gerarCodigo(codSecreto);
+    //printAleatorios(codSecreto);
+    //traduz(codSecreto,code);
+    //imprimir_matriz(matriz);
+    //contador(matriz);
+    jogadafunct(matriz,jogada);
+    //verificarConbinacao(matriz,jogada,codSecreto);
+
+
+
+    //printf("Vamos adivinhar a sequencia!\n");
+    //printf("As letas sao \n---BGRYWO--- \nIndique 4 letras ");
+    //printf("%c %c %c %c \n",matriz[jogada],matriz[jogada+1],matriz[jogada+2],matriz[jogada+3]);
+
 
     return 0;
 }
